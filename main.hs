51ec008340772f71ef0797cbf3cb0e0ecfe7aeaf
Bruno Gava <g.g.bruno.00@gmail.com>
2019-11-07 05:10:12 +0000

    Refatoração de código e comentários.

--- main.hs
+++ main.hs
@@ -1,4 +1,4 @@
---Construa um programa que ordena em ordem ascendente uma lista de listas a partir do tamanho das sublistas. Por exemplo:
+-- 1: Construa um programa que ordena em ordem ascendente uma lista de listas a partir do tamanho das sublistas. Por exemplo:
 
 --mysort [[3,1,2],[2,1],[5,7,1,2],[11]]
 --Output: [[11],[2,1],[3,1,2],[5,7,1,2]]
@@ -16,14 +16,15 @@ bubbleSort [x] = [x]
 bubbleSort (x:y:arrLeft) = bubbleSort(init bubbled) ++ [last bubbled]
   where (smaller,bigger) = if(length x <= length y) then (x, y) else (y, x)
     bubbled = [smaller] ++ bubbleSort (bigger:arrLeft)
--- Defina a função myMap :: (a -> b) -> (a -> b) -> [a] -> [b] que aplica de forma alternada duas funções passadas como argumentos aos elementos de uma lista.
+
+-- 2: Defina a função myMap :: (a -> b) -> (a -> b) -> [a] -> [b] que aplica de forma alternada duas funções passadas como argumentos aos elementos de uma lista.
 --myMap (+3) (*10) [0,1,2,3,4,11]
 
 myMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 myMap _ _ [] = []
 myMap f1 f2 (x1:x2:xs) = f1 x1 : f2 x2 : myMap f1 f2 xs
 
--- A partir da função myMap, defina um função luhn :: [Int] -> Bool que implemente o algoritmo de Luhn para a validações de números de cartão de crédito para códigos de cartão de qualquer tamanho.
+-- 3: A partir da função myMap, defina um função luhn :: [Int] -> Bool que implemente o algoritmo de Luhn para a validações de números de cartão de crédito para códigos de cartão de qualquer tamanho.
 
 luhn :: [Int] -> Bool
 luhn [] = False
